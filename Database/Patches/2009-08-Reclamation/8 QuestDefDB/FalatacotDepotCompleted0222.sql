71871009af00a6b4f72c468fe9a4739eeeb83da3
Starcrusher55 <starcrusher@live.com>
2021-04-16 05:44:06 -0700

    Create FalatacotDepotCompleted0222.sql

---
+++ Database/Patches/2009-08-Reclamation/8 QuestDefDB/FalatacotDepotCompleted0222.sql
@@ -0,0 +1,4 @@
+DELETE FROM `quest` WHERE name = 'FalatacotDepotCompleted0222';
+
+INSERT INTO `quest` (name, min_Delta, max_Solves, message, last_Modified)
+VALUES ('FalatacotDepotCompleted0222', 72000, -1, 'Player wait time for Falatacot Depot', '2021-02-22');
