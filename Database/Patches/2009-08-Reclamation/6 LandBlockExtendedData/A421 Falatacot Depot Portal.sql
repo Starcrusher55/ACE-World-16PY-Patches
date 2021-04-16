068916cab170261bc95fa7e9dd9a652cf6730fc6
Starcrusher55 <starcrusher@live.com>
2021-04-16 07:00:39 -0700

    Create A421 Falatacot Depot Portal.sql

---
+++ Database/Patches/2009-08-Reclamation/6 LandBlockExtendedData/A421 Falatacot Depot Portal.sql
@@ -0,0 +1,5 @@
+DELETE FROM `landblock_instance` WHERE `landblock` = 0xA421;
+
+INSERT INTO `landblock_instance` (`guid`, `weenie_Class_Id`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`, `is_Link_Child`, `last_Modified`)
+VALUES (0x7A421000, 47208, 0xA4210002, 12, 36, 380.93698, 1, 0, 0, 0, False, '2021-02-13 11:45:46'); /* Falatacot Depot Portal*/
+/* @teleloc 0xA4210002 [12.000000 36.000000 380.936981] 1.000000 0.000000 0.000000 0.000000 */
